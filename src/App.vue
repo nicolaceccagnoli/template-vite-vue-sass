0bf6921b0043a837c315fb473218386af3966b81
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-22 14:52:00 +0100

    Add Axios and Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
 
 export default {
     data() {
@@ -17,6 +18,11 @@ export default {
 
     methods: {
 
+    },
+    mounted() {
+        // axios.get().then((response) => {
+        //     console.log(response)
+        // });
     }
 
 }
@@ -36,5 +42,4 @@ export default {
 
 <style lang="scss">
 @use 'assets/scss/main' as *;
-@import 'assets/scss/partials/reset';
 </style>
